db92a4e62291c43c518fb8980825b9848742b2b8
rebecca <becky@strategically.co>
2021-05-28 11:11:26 +0100

    updates to pricing page

--- src/sections/pricing/Faq.js
+++ src/sections/pricing/Faq.js
@@ -16,104 +16,253 @@ const Faq = () => {
         {/* Faqs */}
         {/* Faqs */}
         <div className="row justify-content-center">
-          {/* Single Faq */}
-          <div className="col-lg-6 col-md-10">
-            <div
-              className="d-flex mb-10 mb-lg-17"
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
 
-            >
-              <div className="mr-6">
-                <div className="bg-light-buttercup circle-28 text-white mt-1">
-                  <i className="fa fa-question" />
-                </div>
-              </div>
-              <div className="">
-                <h4 className="font-size-7 text-dark-cloud mb-7">
-                Do you offer revisions?
-                </h4>
-                <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
-            We offer free revisions that don't count against your word count for the month.</p>
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
               </div>
             </div>
-          </div>
-          {/* End Single Faq */}
-          {/* Single Faq */}
-          <div className="col-lg-6 col-md-10">
-            <div
-              className="d-flex mb-10 mb-lg-17"
-
-            >
-              <div className="mr-6">
-                <div className="bg-light-buttercup circle-28 text-white mt-1">
-                  <i className="fa fa-question" />
-                </div>
-              </div>
-              <div className="">
-                <h4 className="font-size-7 text-dark-cloud mb-7">
-                  How does it work?
-                </h4>
-                <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
-                Submit your content requests, specify your content requirements, word counts & any research requests. We will select one of our expert writers based on your niche. Once the content is created, the article is emailed to you for feedback & revisions.
-              </p>
-              </div>
+            <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+              Do you offer revisions?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+          We want you to love your content. Our clients rarely use them, but we do offer unlimited free revisions that don't count against your word count for the month.</p>
             </div>
           </div>
-          {/* End Single Faq */}
-          {/* Single Faq */}
-          <div className="col-lg-6 col-md-10">
-            <div
-              className="d-flex mb-10 mb-lg-17"
-
-            >
-              <div className="mr-6">
-                <div className="bg-light-buttercup circle-28 text-white mt-1">
-                  <i className="fa fa-question" />
-                </div>
-              </div>
-              <div className="">
-                <h4 className="font-size-7 text-dark-cloud mb-7">
-                  Do you offer any trials or samples?
-                </h4>
-                <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
-              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">We offer a try-before-you-buy option so you can see your ideas come to life using our writers. We also have sample content and content strategies available. Please <a href='mailto:hello@strategically.co'>email us</a>.</p>
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End Single Faq */}
-          {/* Single Faq */}
-          <div className="col-lg-6 col-md-10">
-            <div
-              className="d-flex mb-10 mb-lg-17"
-
-            >
-              <div className="mr-6">
-                <div className="bg-light-buttercup circle-28 text-white mt-1">
-                  <i className="fa fa-question" />
-                </div>
-              </div>
-              <div className="">
-                <h4 className="font-size-7 text-dark-cloud mb-7">Do you have a minimum contract length?</h4>
-                <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
-We recommend a minimum of three months in order to see how content can help you reach your goals.
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* End Single Faq */}
         </div>
-        {/* Button  */}
-        <div
-          className="text-center pt-lg-3 pb-lg-3"
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
 
-        >
-          <p className="font-size-6 mb-0">
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+            <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+                How does it work?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+              Submit your content requests, specify your content requirements, word counts & any research requests. We will select one of our expert writers based on your niche. Once the content is created, the article is emailed to you for feedback & revisions.
+            </p>
+            </div>
+          </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+            <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+                Do you offer any trials or samples?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">We offer a try-before-you-buy option so you can see your ideas come to life using our writers. We charge 20 cents per word for trials.</p>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+<div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+                Do you have experience writing for our industry?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">We are a team of expert writers and editors. Our writers have written for a wide variety of websites and industries. When you sign up, we assign you a writer that is best suited to your niche. </p>
+              </p>
+            </div>
+
+          </div>
+        </div>
+        {/* End Single Faq */}
+
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+             <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+               How many blogs do I get in each package?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">It's totally up to you. Most of our clients request articles of either 500, 750, 1000, 2000 words each.  At the start of the month, please send us a list of titles and briefs (if you have them) and suggested word counts for each. We then deliver as many articles as we can within the number of words.</p>
+              </p>
+            </div>
+         </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+            <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+              What is your minimum commitment?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">We can do ad-hoc work at 20 cents per word. Alternatively, if you sign up for a monthly package, and you’re not happy (you will be!), you can cancel at the end of the month.</p>
+              </p>
+            </div>
+         </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+  <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+              What is your typical turnaround time?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Day 1 - 5: Titles provided by the client</p>
+<p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Day 6 - 20: We write and edit the content</p>
+<p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Day 1 - 5: Day 21 - 28: Any feedback/revisions</p>
+<p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Please note, if we do not receive the titles by Day 5, these timelines may be delayed. We do ask for your support with this. </p>
+              </p>
+            </div>
+         </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+
+<div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+             Are you able to also suggest article titles?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+<p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Yes, we can also suggest titles. Monthly package clients can purchase title suggestions for $15 per blog. For example, if you’re on the Starter package and would like 4 x 1000 word blogs, you would pay an additional $60 per month. The title recommendation would include title suggestion, keyword clusters, and an outline for the article. </p>
+
+              </p>
+            </div>
+
+          </div>
+        </div>
+        {/* End Single Faq */}
+
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+  <div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+              What about images?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+            <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">If you would like images, we can include free stock images and/or relevant screenshots in the article.</p>
+
+              </p>
+            </div>
+         </div>
+        </div>
+        {/* End Single Faq */}
+        {/* Single Faq */}
+        <div className="col-lg-6 col-md-10">
+          <div
+            className="d-flex mb-10 mb-lg-17"
+
+          >
+            <div className="mr-6">
+              <div className="bg-light-buttercup circle-28 text-white mt-1">
+                <i className="fa fa-question" />
+              </div>
+            </div>
+
+<div className="">
+              <h4 className="font-size-7 text-dark-cloud mb-7">
+             Do you offer SEO content strategies?
+              </h4>
+              <p className="font-size-5 pr-md-10 pr-lg-0 mb-0">
+<p className="font-size-5 pr-md-10 pr-lg-0 mb-0">Yes, we have SEO experts on the team. We offer a 6 month SEO content strategy for a one-time fee of $999. Alternatively, if you sign up for a 6 month content contract, we include the strategy free of charge. </p>
+
+              </p>
+            </div>
+
+          </div>
+        </div>
+        {/* End Single Faq */}
+      </div>
+      {/* Button  */}
+
+
+      <div
+        className="text-center pt-lg-3 pb-lg-3"
+
+      >
+        <p className="font-size-6 mb-0">
 If you are an agency or an enterprise customer, our packages scale with your needs. Please <a href='mailto:hello@strategically.co'>email us</a> for a custom quote.</p>
 
-        </div>
-        {/* Button End */}
       </div>
+      {/* Button End */}
     </div>
+  </div>
   );
 };
 
