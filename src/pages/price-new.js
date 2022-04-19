82ebc66bb830c195039855893cd3f78e3b35eb77
nanirama <nani.rama1982@gmail.com>
2022-04-19 16:31:42 +0530

    New Pricing page changes

--- src/pages/price-new.js
+++ src/pages/price-new.js
@@ -186,6 +186,38 @@ const PricePage = ({ data }) => {
         <div className="container-fluid mt-20 pt-20 mb-10">
         <HappyClients />
         </div>
+        <div className="bg-dark-cloud pt-20 pt-lg-20 pb-15 pb-lg-20">
+          <div className="container">
+            <div className="row d-flex justify-content-center">
+             
+              <div className="col-xl-9 col-lg-9 col-md-10 mt-n13 mt-md-n8 mt-lg-n0 d-flex" >
+                <div
+                  className="dark-mode-texts"
+                  data-aos="fade-left"
+                  data-aos-duration={600}
+                  data-aos-delay={500}
+                  data-aos-once="true"
+                >
+           
+                  <h2 className="font-size-10 mb-8 letter-spacing-n83 pr-xs-22 pr-sm-18 pr-md-0 text-center">
+                  Interested in SEO content creation? Get in touch. 
+                  </h2>
+                  <p className="font-size-7 mb-0 text-center">
+                  Book a call with me, Becky. I love to chat :)
+                  </p>
+                  <div className="d-flex justify-content-center">
+                  <a
+                      className="btn btn btn-dodger-blue-2 header-btn rounded-pill mt-10"
+                      href="https://calendly.com/becky-strategically/intro-meeting" target="_blank"
+                    >
+                      {buttonText}
+                    </a>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </PageWrapper>
     </>
   );
