9e8b9c6a4be866a7dc20ded5ef49e6b84e867b84
nanirama <nani.rama1982@gmail.com>
2022-05-13 08:56:14 +0530

    Images redirect bug fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,58 @@ const _ = require(`lodash`);
 const { paginate } = require(`gatsby-awesome-pagination`);
 
 exports.onCreatePage = ({ page, actions }) => {
-  const { createPage } = actions;
+  const { createPage, createRedirect } = actions;
+
+  const  rurls = [
+    {oldurl:'/writer-services/writer-services/',newurl: '/writer-services/'},
+    {oldurl:'/writer-services/seo-content-writing/images/mob-banner.jpg',newurl: '/writer-services/seo-content-writing/'},
+    {oldurl:'/writer-services/b2b-content-writing-services/images/mob-banner.jpg',newurl: '/writer-services/b2b-content-writing-services/'},
+    {oldurl:'/writer-services/fintech-content-writer/images/mob-banner.jpg',newurl: '/writer-services/fintech-content-writer/'},
+    {oldurl:'/writer-services/blog-article-writing/images/mob-banner.jpg',newurl: '/writer-services/blog-article-writing/'},
+    {oldurl:'/writer-services/images/mob-banner.jpg',newurl: '/writer-services/'},
+    {oldurl:'/writer-services/saas-content-writer/images/mob-banner.jpg',newurl: '/writer-services/saas-content-writer/'},
+    {oldurl:'/writer-services/marketing-content-writer/images/mob-banner.jpg',newurl: '/writer-services/marketing-content-writer/'},
+    {oldurl:'/writer-services/white-paper-writing/images/mob-banner.jpg',newurl: '/writer-services/white-paper-writing/'},
+    {oldurl:'/writer-services/b2b-content-marketing-agency/images/mob-banner.jpg',newurl: '/writer-services/b2b-content-marketing-agency/'},
+    {oldurl:'/writer-services/product-review-writing/images/mob-banner.jpg',newurl: '/writer-services/product-review-writing/'},
+    {oldurl:'/writer-services/food-writer/images/mob-banner.jpg',newurl: '/writer-services/food-writer/'},
+    {oldurl:'/writer-services/automotive-content-marketing-writer/images/mob-banner.jpg',newurl: '/writer-services/automotive-content-marketing-writer/'},
+    {oldurl:'/writer-services/beauty-writer/images/mob-banner.jpg',newurl: '/writer-services/beauty-writer/images/mob-banner.jpg'},
+    {oldurl:'/writer-services/insurance-writer/images/mob-banner.jpg',newurl: '/writer-services/insurance-writer/'},
+    {oldurl:'/writer-services/health-writer/images/mob-banner.jpg',newurl: '/writer-services/health-writer/'},
+    {oldurl:'/writer-services/nutrition-writer/images/mob-banner.jpg',newurl: '/writer-services/nutrition-writer/'},
+    {oldurl:'/writer-services/product-description-writing/images/mob-banner.jpg',newurl: '/writer-services/product-description-writing/'},
+    {oldurl:'/writer-services/fitness-writer/images/mob-banner.jpg',newurl: '/writer-services/fitness-writer/'},
+    {oldurl:'/writer-services/human-resources-writer/images/mob-banner.jpg',newurl: '/writer-services/human-resources-writer/'},
+    {oldurl:'/writer-services/cyber-security-writer/images/mob-banner.jpg',newurl: '/writer-services/cyber-security-writer/'},
+    {oldurl:'/writer-services/cannabis-seo-content-writer/images/mob-banner.jpg',newurl: '/writer-services/cannabis-seo-content-writer/'},
+    {oldurl:'/writer-services/financial-writing/images/mob-banner.jpg',newurl: '/writer-services/financial-writing/'},
+    {oldurl:'/writer-services/fashion-writer/images/mob-banner.jpg',newurl: '/writer-services/fashion-writer/'},
+    {oldurl:'/writer-services/medical-healthcare-writing/images/mob-banner.jpg',newurl: '/writer-services/medical-healthcare-writing/'},
+    {oldurl:'/writer-services/construction-content-marketing-writer/images/mob-banner.jpg',newurl: '/writer-services/construction-content-marketing-writer/'},
+    {oldurl:'/writer-services/dental-content-writing/images/mob-banner.jpg',newurl: '/writer-services/dental-content-writing/'},
+    {oldurl:'/writer-services/ecommerce-content-marketing-writer/images/mob-banner.jpg',newurl: '/writer-services/ecommerce-content-marketing-writer/'},
+    {oldurl:'/writer-services/travel-writing/images/mob-banner.jpg',newurl: '/writer-services/travel-writing/'},
+    {oldurl:'/writer-services/garden-floral-writer/images/mob-banner.jpg',newurl: '/writer-services/garden-floral-writer/'},
+    {oldurl:'/writer-services/casino-gaming-content-writing/images/mob-banner.jpg',newurl: '/writer-services/casino-gaming-content-writing/'},
+    {oldurl:'/writer-services/education-content-writing/images/mob-banner.jpg',newurl: '/writer-services/education-content-writing/'},
+    {oldurl:'/writer-services/legal-writing/images/mob-banner.jpg',newurl: '/writer-services/legal-writing/'},
+    {oldurl:'/writer-services/mental-health-writer/images/mob-banner.jpg',newurl: '/writer-services/mental-health-writer/'},
+    {oldurl:'/writer-services/real-estate-writer/images/mob-banner.jpg',newurl: '/writer-services/real-estate-writer/'},
+    {oldurl:'/writer-services/ebook-writing/images/mob-banner.jpg',newurl: '/writer-services/ebook-writing/'},
+    {oldurl:'/writer-services/accounting-writer/images/mob-banner.jpg',newurl: '/writer-services/accounting-writer/'},
+  ]
+
+  rurls.forEach((item) => {     
+    createRedirect({
+      fromPath: item.oldurl,
+      toPath: item.newurl,
+      redirectInBrowser: true,
+      isPermanent: true,
+    });
+  })
+  
+
  if (page.path.match(/sign|reset/)) {
     page.context.layout = "mini";
     createPage(page);
