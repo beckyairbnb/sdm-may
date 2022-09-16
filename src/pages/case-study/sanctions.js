e2bdc7e0d0388caed0fa2bd8ee712d413f3dd0cd
nanirama <nani.rama1982@gmail.com>
2022-09-16 20:58:50 +0530

    cschanges

--- src/pages/case-study/sanctions.js
+++ src/pages/case-study/sanctions.js
@@ -8,7 +8,7 @@ import PageWrapper from "../../components/PageWrapper";
 import Icon1 from "../../assets/images/icon-traffic.png";
 import Icon2 from "../../assets/images/icon-value.png";
 import Icon3 from "../../assets/images/icon-links.png";
-import ClientImg from "../../assets/images/sanction-img.png";
+import ClientImg from "../../assets/images/lanacooper-2.jpg";
 
 import FeaturedImg5 from "../../assets/images/post_img1.jpg"
 import FeaturedImg6 from "../../assets/images/post_img2.jpg"
@@ -43,6 +43,31 @@ const Sanctions = () => {
                         </div>
                     </div>
                 </div>
+
+                <div class="bg-dark-cloud w-full py-md-20 py-14  text-center">
+                    <div class="container">
+                        <div class="row d-flex justify-content-center align-items-center">
+                            <div class="col-md-4 col-sm-6 mb-5">
+                                <img src={Icon2} alt="" />
+                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">0 to 2.5M</h2> */}
+                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">3X increase </h2>
+                                <h5 className="text-center text-white">in organic traffic</h5>
+                            </div>
+                            <div class="col-md-4 col-sm-6 mb-5">
+                                <img src={Icon1} alt="" />
+                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">1.7M</h2> */}
+                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">2.5X words</h2>
+                                <h5 className="text-center text-white">written per month</h5>
+                            </div>
+                            <div class="col-md-4 col-sm-6 mb-5">
+                                <img src={Icon3} alt="" />
+                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">7,000+</h2> */}
+                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3"> 4 agencies</h2>
+                                <h5 className="text-center text-white">tried & failed</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
 
                 <div className="py-12 pt-lg-20 text-size">
@@ -50,10 +75,10 @@ const Sanctions = () => {
                         <div class="row justify-content-center">
                             <div class="col-xl-12 col-12">
                                 <div class="row mb-lg-14 mb-10">
-                                    <div class="col-lg-4 col-12">
+                                    <div class="col-lg-5 col-12 cimage">
                                         <img className="w-full justify-center mb-10" src={ClientImg} alt="" />
                                     </div>
-                                    <div class="col-lg-8 col-md-11">
+                                    <div class="col-lg-7 col-md-11 mt-10">
                                         <h2 className="font-size-10">The client</h2>
                                         <div className="service-post-divider "></div>
                                         <p><a href="https://sanctions.io/">Sanctions.io</a> knows all there is about business partner screenings against global sanctions. The start-up has helped 100+ companies with their simple and reliable data and technology solutions. But co-founders Thorsten, Hector, and Jens were struggling in one department: content creation.</p>
@@ -94,60 +119,10 @@ const Sanctions = () => {
 
 
                 </div>
-                {/* <div class="quote-blk py-10 py-lg-22 text-center">
-                    <div className="container">
-                        <h2 className="text-white font-size-md-12 font-size-10  my-0">“With Verblio, it's trackable. You charge me by post—simple as that. You don't care how long it takes. You charge me by the number of words.”</h2>
-                    </div>
-                </div> */}
-
-
-                {/* <div class="bg-default-3 pt-12 pt-md-18 pb-lg-10 pb-5 pt-lg-22 text-center">
-                    <div className="container">
-                        <h2 className="font-size-10 mb-md-14 mb-8">Featured Content</h2>
-                        <div class="row">
-                            <div class="col-md-6 mb-md-12 mb-8">
-                                <a href="#">
-                                    <img className="w-100 mb-8" src={FeaturedImg1} alt="" />
-                                    <h3 className="font-size-9">Wedding Hashtag Interactive</h3>
-                                    <p class="teaser-subtitle font-size-8">Ranking #1 for “mother's day quotes," Est. 26,400,000 visitors/year, 184 links</p>
-                                </a>
-                            </div>
-                            <div class="col-md-6 mb-md-12 mb-8">
-                                <a href="#">
-                                    <img className="w-100 mb-8" src={FeaturedImg2} alt="" />
-                                    <h3 className="font-size-9">Mother's Day Quotes</h3>
-                                    <p class="teaser-subtitle font-size-8">Ranking #1 for “mother's day quotes," Est. 26,400,000 visitors/year, 184 links</p>
-                                </a>
-                            </div>
-                            <div class="col-md-6 mb-md-12 mb-8">
-                                <a href="#">
-                                    <img className="w-100 mb-8" src={FeaturedImg3} alt="" />
-                                    <h3 className="font-size-9">Difference Between Ms. and Mrs.</h3>
-                                    <p class="teaser-subtitle font-size-8">Ranking #1, Est. 48,700 visits/month, 72 links</p>
-                                </a>
-                            </div>
-                            <div class="col-md-6 mb-md-12 mb-8">
-                                <a href="#">
-                                    <img className="w-100 mb-8" src={FeaturedImg4} alt="" />
-                                    <h3 className="font-size-9">Graduation Party Ideas for Your Celebration</h3>
-                                    <p class="teaser-subtitle font-size-8">260,000+ YouTube views</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
 
 
                 <div class="pb-10 text-size">
                     <div className="container">
-
-                        {/* <h2 className="font-size-10 mb-lg-10 mb-5">Growth Summary</h2>
-
-                        <p>product lines spanning wedding, graduation, babies and the holiday season. Through high-quality content generated over several years, we helped Shutterfly significantly build their traffic and sales.</p>
-                        <p>We flexed our wide range of SEO-focused content creation capabilities, creating interactives, videos, product photography, blog articles and more.</p>
-                        <p>Given Shutterfly had strong existing authority, most of the link building performed was done through passive link acquisition via the rankings and traffic generated. We ran the content strategy and creation end-to-end and actively scaled up content for Shutterfly, creating more than 500 unique pieces over three years to power the company to over 2,500,000 monthly visits.</p>*/}
-
-
                         <h2 className="font-size-10 mb-lg-10 mb-5">The solution</h2>
                         <p> After yet another dissatisfied experience with a content writing agency, Thorsten took to Google. He searched again for an agency that could be up for the challenge of producing niche content for their blog. This time, he stumbled across Strategically.</p>
                         <p>Thorsten knew we were the one: <span class="purple">“it was just a feeling.”</span> and we offered an affordable pricing plan aligned with his smaller startup budget. Thorsten said our <span className="purple"> “general offering for a small start-up like ours is very attractive from a pricing standpoint.”</span></p>
@@ -195,30 +170,7 @@ const Sanctions = () => {
                         
                     </div> */}
                 </div>
-                <div class="bg-dark-cloud w-full py-md-20 py-14  text-center">
-                    <div class="container">
-                        <div class="row d-flex justify-content-center align-items-center">
-                            <div class="col-md-4 col-sm-6 mb-5">
-                                <img src={Icon2} alt="" />
-                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">0 to 2.5M</h2> */}
-                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">3X increase </h2>
-                                <h5 className="text-center text-white">in organic traffic</h5>
-                            </div>
-                            <div class="col-md-4 col-sm-6 mb-5">
-                                <img src={Icon1} alt="" />
-                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">1.7M</h2> */}
-                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">2.5X words</h2>
-                                <h5 className="text-center text-white">written per month</h5>
-                            </div>
-                            <div class="col-md-4 col-sm-6 mb-5">
-                                <img src={Icon3} alt="" />
-                                {/* <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3">7,000+</h2> */}
-                                <h2 class="font-size-lg-12 font-size-10 text-center text-white mt-3"> 4 agencies</h2>
-                                <h5 className="text-center text-white">tried & failed</h5>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+               
 
                 
 
