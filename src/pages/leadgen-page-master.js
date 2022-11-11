f09fb125446dde8d8bb57d24de740bdf93190969
nanirama <nani.rama1982@gmail.com>
2022-11-11 11:05:21 +0530

    Ledgen page adde

--- src/pages/leadgen-page-master.js
+++ src/pages/leadgen-page-master.js
@@ -1,62 +1,31 @@
 
 import React, { useState, useEffect } from 'react';
 import { Helmet } from "react-helmet";
+import { useForm } from "react-hook-form";
 import LeadgenLayout from "../components/Layout/LeadgenLayout";
 const LeadgenPageMaster = () => {
-    const [formValid, setFormValid] = useState(false)
-    const [inputValues, setInputValue] = useState({
-        fullName: '',
-        email: '',
-        phone: ''
-      });
-    
-      const [validation, setValidation] = useState({
-        fullName: '',
-        email: '',
-        phone: ''
-      });    
-      
-      function handleChange(event) {
-        const { name, value } = event.target;
-        setInputValue({ ...inputValues, [name]: value });
-        handleAfterClick(event)
-      }
-    
-      const handleAfterClick = (e) => {
-        e.preventDefault();
-        let errors = validation;
-        if (inputValues.fullName==='') {
-          errors.fullName = 'FullName is required';
-        } else {
-          errors.fullName = '';
-        }
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-        const emailCond = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
-        if (inputValues.email==='') {
-          errors.email = 'Email is required';
-        } else if (inputValues.email.match(emailCond)) {
-          errors.email = 'Please enter a valid email address';
-        } else {
-          errors.email = '';
-        }
-
-        if (inputValues.phone==='') {
-            errors.phone = 'Phone Number is required';
-          } else {
-            errors.phone = '';
-          }   
-          if(errors.fullName==='' && errors.email==='' && errors.phone===''){
-            setFormValid(true)  
-          }
-          console.log('errors',errors)
-        return setValidation(errors);
-      };
-      useEffect(() => {        
-                console.log('formValid 4',formValid)
-                console.log('validation 3',validation)
-                console.log('inputValues 2',inputValues)
-        }, [handleAfterClick, inputValues, validation, formValid]);
-
+    const onSubmit = (data, e) => {
+        const form = e.target
+        const formData = new FormData(form);
+        e.preventDefault()
+        
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: JSON.stringify({
+                'form-name': 'LeadgenMaster',
+                body: data,
+            }),
+        })
+            .then(response => {                
+                console.log('succes',response)
+            })
+            .catch(error => {
+                console.log('failed',error)
+            })
+    }
     return (
         <>
             <Helmet>
@@ -71,29 +40,27 @@ const LeadgenPageMaster = () => {
                             Enter your account details below
                         </p>
                     </div>
-                    <form name="LeadgenMaster" method="POST" data-netlify="true" action={`/success`}
-                        data-aos="fade-up"
-                        data-aos-duration={800}
-                        data-aos-delay={500}
-                    >
-                        <input type="hidden" name="form-name" value="LeadgenMaster" />
-                        {/* <FormErrors formErrors={this.state.formErrors} /> */}
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        name="LeadgenMaster"
+                        method="POST"
+                        data-netlify="true"
+                        action="/"
+                        id="LeadgenMaster"
+                     >
+                    <input type="hidden" name="form-name" value="LeadgenMaster" />
                         <div className="form-group position-relative mb-6">
                             <input
                                 type="text"
                                 name="fullName"
                                 placeholder="fullName"
                                 id="fullName"
-                                onBlur={(e) => handleChange(e)}
-                                onChange={(e) => handleChange(e)}
-                                value={inputValues.fullName}
+                                {...register("fullName", { required: true })}
                                 className=
                                 {
-                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${validation && validation.fullName && 'error'}`
+                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${errors.fullName && 'error'}`
                                 }
                             />
-                            {validation.fullName && <p>{validation.fullName}</p>}
-                            {validation.fullName && console.log(validation)}
                         </div>
                         {/* Email */}
                         <div className="form-group mb-6 position-relative">
@@ -102,14 +69,12 @@ const LeadgenPageMaster = () => {
                                 name="email"
                                 placeholder="Email Address"
                                 id="email"
-                                onChange={(e) => handleChange(e)}
-                                value={inputValues.email}
+                                {...register("email", { required: true })}
                                 className=
                                 {
-                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${validation && validation.email && 'error'}`
+                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${errors.email && 'error'}`
                                 }
                             />
-                            {validation.email && <p>{validation.email}</p>}
                         </div>
 
                         <div className="form-group position-relative mb-6">
@@ -118,21 +83,18 @@ const LeadgenPageMaster = () => {
                                 name="phone"
                                 placeholder="Phone Number"
                                 id="phone"
-                                onChange={(e) => handleChange(e)}
-                                value={inputValues.phone}
+                                {...register("phone", { required: true })}
                                 className=
                                 {
-                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${validation && validation.phone && 'error'}`
+                                    `form-control form-control-lg bg-white rounded-4 text-dark-cloud text-placeholder-bali-gray pl-7 font-size-5 ${errors.phone && 'error'}`
                                 }
                             />
-                            {validation.phone && <p>{validation.phone}</p>}
                         </div>
 
 
                         <div className="button">
-                        <button type="submit" className="btn btn-blue-3  w-100 rounded-4"
-                        disabled={!formValid}
-                        onClick={handleAfterClick}>Continue</button>
+                        <input type="submit" value="Continue" className="btn btn-blue-3  w-100 rounded-4" />
+    
                         </div>
                     </form>
             </LeadgenLayout>
