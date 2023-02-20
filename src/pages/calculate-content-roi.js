095bc77da41e4a20dc20414af248d6a723d892a9
nanirama <nani.rama1982@gmail.com>
2023-02-20 18:57:10 +0530

    add Attributes code

--- src/pages/calculate-content-roi.js
+++ src/pages/calculate-content-roi.js
@@ -35,6 +35,13 @@ const LeadgenPageMaster = () => {
             >
                 <NetlifyFormProvider {...netlify}>
                     <NetlifyFormComponent onSubmit={handleSubmit(onSubmit)}>
+                    <input type="hidden" id="[attributer-channel]" name="[attributer-channel]" value="[channel]"/>
+                        <input type="hidden" id="[attributer-channeldrilldown1]" name="[attributer-channeldrilldown1]" value="[channeldrilldown1]"/>
+                        <input type="hidden" id="[attributer-channeldrilldown2]" name="[attributer-channeldrilldown2]" value="[channeldrilldown2]"/>
+                        <input type="hidden" id="[attributer-channeldrilldown3]" name="[attributer-channeldrilldown3]" value="[channeldrilldown3]"/>
+                        <input type="hidden" id="[attributer-channeldrilldown4]" name="[attributer-channeldrilldown4]" value="[channeldrilldown4]"/>
+                        <input type="hidden" id="[attributer-landingpage]" name="[attributer-landingpage]" value="[landingpage]"/>
+                        <input type="hidden" id="[attributer-landingpagegroup]" name="[attributer-landingpagegroup]" value="[landingpagegroup]"/>
                     <div className="form-group position-relative mb-4">
                             <input
                                 type="text"
