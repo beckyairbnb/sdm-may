8d9b4f749fc651a48aa538d95b23158504e007ee
nanirama <nani.rama1982@gmail.com>
2023-02-23 17:57:12 +0530

    file added at bottom

--- src/html.js
+++ src/html.js
@@ -31,9 +31,9 @@ export default function HTML(props) {
         (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);
         `}
         </script>          
-        <noscript><img height="1" width="1" style={{display:"none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=4460818&fmt=gif" /></noscript>        
-        <script type="text/javascript" src="https://d1b3llzbo1rqxo.cloudfront.net/attributer.js"></script> 
+        <noscript><img height="1" width="1" style={{display:"none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=4460818&fmt=gif" /></noscript>                
       </body>
+      <script type="text/javascript" src="https://d1b3llzbo1rqxo.cloudfront.net/attributer.js"></script> 
     </html>
   )
 }
