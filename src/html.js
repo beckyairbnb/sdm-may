40266c86fd4e54f1275ce570ecff63e20ba581c4
Andru <andru@tinymighty.com>
2023-03-06 15:30:24 +0000

    fix: use dangerouslySetInnerHTML to define body scripts in Gatsby SSR html.js

--- src/html.js
+++ src/html.js
@@ -23,16 +23,12 @@ export default function HTML(props) {
         />
         <script type="text/javascript" src="https://d1b3llzbo1rqxo.cloudfront.net/attributer.js"></script>
         {props.postBodyComponents}
-        <script>
-        {`
-        _linkedin_partner_id = "4460818"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        `}
-        </script>  
-        <script>
-        {`
-        (function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);
-        `}
-        </script>          
+        <script dangerouslySetInnerHTML={{__html:
+          `_linkedin_partner_id = "4460818"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id);`
+        }} />  
+        <script dangerouslySetInnerHTML={{__html:
+          `(function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])}; window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(window.lintrk);`
+        }} />          
         <noscript><img height="1" width="1" style={{display:"none"}} alt="" src="https://px.ads.linkedin.com/collect/?pid=4460818&fmt=gif" /></noscript>                
         
       </body>
